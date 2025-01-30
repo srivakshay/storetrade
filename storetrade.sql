3412d7947e2fd8d2c89d911cd510fb39f92336a8
Akshay Srivastava <akshay@neuartworks.com>
2025-01-30 06:31:51 +0530

    trade store code

---
+++ storetrade.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2025 at 02:00 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `storetrade`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trade`
+--
+
+CREATE TABLE `trade` (
+                         `id` bigint(20) NOT NULL,
+                         `symbol` varchar(50) NOT NULL,
+                         `trade_type` varchar(50) NOT NULL,
+                         `price` decimal(38,2) NOT NULL,
+                         `quantity` int(11) NOT NULL,
+                         `trade_time` datetime NOT NULL,
+                         `version` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trade_seq`
+--
+
+CREATE TABLE `trade_seq` (
+                             `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `trade_seq`
+--
+
+INSERT INTO `trade_seq` (`next_val`) VALUES
+    (301);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
